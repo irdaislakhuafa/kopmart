dbb820c84ec5053d856b7771c1d850cfec3dae71
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-01-21 13:03:45 +0700

    first commit

---
+++ src/main/java/com/irdaislakhuafa/kopmart/KopmartApplication.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.kopmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irdaislakhuafa/kopmart/KopmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.kopmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
