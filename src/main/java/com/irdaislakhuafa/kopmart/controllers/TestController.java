7a4ceaffc04a2b7443af48ab741f9b5ba71b1314
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-02-22 01:02:55 +0700

    create download sample file CSV for data category

--- src/main/java/com/irdaislakhuafa/kopmart/controllers/TestController.java
+++ src/main/java/com/irdaislakhuafa/kopmart/controllers/TestController.java
@@ -1,5 +1,14 @@
 package com.irdaislakhuafa.kopmart.controllers;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.irdaislakhuafa.kopmart.helpers.DataToCsvHelper;
 import com.irdaislakhuafa.kopmart.services.KeranjangService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +23,25 @@ public class TestController {
     private KeranjangService keranjangService;
 
     @GetMapping
-    public String name() {
+    public String name() throws Exception {
+        List<Map<String, Object>> listData = new ArrayList<>(
+                Arrays.asList(
+                        new HashMap<String, Object>() {
+                            {
+                                put("nama", "irda islakhu afa");
+                                put("noTelp", "082244786497");
+                                put("mLaki", true);
+                            }
+                        },
+                        new HashMap<String, Object>() {
+                            {
+                                put("nama", "ana ardani");
+                                put("noTelp", "089668062233");
+                                put("mLaki", false);
+                            }
+                        }));
+
+        DataToCsvHelper.writeDataToCsv(new PrintWriter(new File("/home/artix/.cache/statusInet.log")), listData);
         return "Hello World";
     }
 }
